20175c7b9d2a4f9b52a1c9da2a86d8cdf17f21ca
L3monPL <mikolaj.andrzejewski999@gmail.com>
2022-06-22 23:02:39 +0200

    https://github.com/L3monPL/sitereact

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
           <div className="header_title">
             <div className="header_box_text">
               <h1>Siłownia Strongman</h1>
-              <p>Sprawdz nasze ceny karnetów, jesteśmy najwiekszą społecznością w okolicy. Przyjmujemy karnet sport +</p>
+              <p>Sprawdz nasze ceny karnetów, jesteśmy najwiekszą społecznością w okolicy. Przyjmujemy karnet multisport</p>
               <div className="header_buttons_container">
                 <a href="#plans" className='header_btn header_btn_1 header_btn_hover'>Cennik</a>
                 <a href="#map" className='header_btn header_btn_1_hover'>Dojazd</a>
